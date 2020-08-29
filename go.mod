c47857a5f849b5843d4142cef77a9ff2fd90e040
yayanyang <org.lemon@gmail.com>
2020-08-29 22:04:59 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/libs4go/ipfslog-slf4go
+
+go 1.14
+
+require github.com/ipfs/go-log/v2 v2.1.1
---
+++ ipfslog.go
@@ -0,0 +1,81 @@
+package slf4goipfs
+
+import (
+	"encoding/json"
+
+	ipfslog "github.com/ipfs/go-log/v2"
+	"github.com/libs4go/slf4go"
+)
+
+func init() {
+	ipfslog.SetupLogging(ipfslog.Config{})
+	ipfslog.SetAllLoggers(ipfslog.LevelDebug)
+
+	reader := ipfslog.NewPipeReader()
+
+	go redirectLoop(reader)
+}
+
+type loggerJSON struct {
+	Level   string `json:"level"`
+	TS      string `json:"ts"`
+	Logger  string `json:"logger"`
+	Message string `json:"msg"`
+}
+
+var errlog = slf4go.Get("ipfslog")
+
+type logF func(message string, args ...interface{})
+
+func getLogF(level string, logger slf4go.Logger) logF {
+	l, err := ipfslog.LevelFromString(level)
+
+	if err != nil {
+		errlog.E("decode ipfs log level {@level} error {@error}", level, err)
+
+		return logger.I
+	}
+
+	switch l {
+	case ipfslog.LevelDebug:
+		return logger.D
+	case ipfslog.LevelInfo:
+		return logger.I
+	case ipfslog.LevelWarn:
+		return logger.W
+	case ipfslog.LevelError:
+		return logger.E
+	default:
+		return logger.E
+	}
+}
+
+func redirectLoop(reader *ipfslog.PipeReader) {
+
+	logs := make(map[string]slf4go.Logger)
+
+	for {
+		decoder := json.NewDecoder(reader)
+
+		var logEntry loggerJSON
+
+		err := decoder.Decode(&logEntry)
+
+		if err != nil {
+			errlog.E("decode ipfs log error {@error}", err)
+			continue
+		}
+
+		log, ok := logs[logEntry.Logger]
+
+		if !ok {
+			log = slf4go.Get(logEntry.Logger)
+
+			logs[logEntry.Logger] = log
+		}
+
+		f := getLogF(logEntry.Level, log)
+
+		f(logEntry.Message)
+	}
+}
